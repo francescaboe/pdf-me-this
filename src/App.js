22a785f460b769fd31838b9e3d925183ad63e04d
francescaboe <francesca.boe@phiture.com>
2023-05-22 21:38:16 +0200

    adds first draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { PDFDocument, StandardFonts } from 'pdf-lib';
+import { Button, Container, Typography, Link } from '@mui/material';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [pdfFiles, setPdfFiles] = useState([]);
+    const [combinedPdfBlob, setCombinedPdfBlob] = useState(null);
+
+    const handleFileChange = (event) => {
+        const files = Array.from(event.target.files);
+        setPdfFiles((prevFiles) => [...prevFiles, ...files]);
+    };
+
+    const handleCombinePDFs = async () => {
+        const pdfDoc = await PDFDocument.create();
+
+        for (const file of pdfFiles) {
+            const fileData = await file.arrayBuffer();
+            const filePdf = await PDFDocument.load(fileData);
+            const copiedPages = await pdfDoc.copyPages(filePdf, filePdf.getPageIndices());
+            copiedPages.forEach((page) => {
+                pdfDoc.addPage(page);
+            });
+        }
+
+        const combinedPdfBytes = await pdfDoc.save();
+        const combinedPdfBlob = new Blob([combinedPdfBytes], { type: 'application/pdf' });
+        setCombinedPdfBlob(combinedPdfBlob);
+    };
+
+    const handleReset = () => {
+        setPdfFiles([]);
+        setCombinedPdfBlob(null);
+    };
+
+    return (
+        <Container maxWidth="sm">
+            <Typography variant="h4" align="center" gutterBottom>
+                PDF Combiner
+            </Typography>
+            <input type="file" multiple onChange={handleFileChange} />
+            <Button variant="contained" onClick={handleCombinePDFs} disabled={pdfFiles.length === 0}>
+                Combine PDFs
+            </Button>
+            {combinedPdfBlob && (
+                <div>
+                    <Typography variant="h6" align="center" gutterBottom>
+                        Combined PDF
+                    </Typography>
+                    <Link href={URL.createObjectURL(combinedPdfBlob)} download="combined.pdf">
+                        Download Combined PDF
+                    </Link>
+                </div>
+            )}
+            {pdfFiles.length > 0 && (
+                <Button variant="outlined" onClick={handleReset}>
+                    Reset
+                </Button>
+            )}
+        </Container>
+    );
 }
 
 export default App;
